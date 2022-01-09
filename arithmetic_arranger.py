d1b02edad1c67f56fa6bda96bd24a00cd13ccbd5
Aryan Agrawal <alemantrixaryan@gmail.com>
2022-01-09 18:11:07 +0530

    Its working but not good to show to the public

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,4 +1,5 @@
-def arithmetic_arranger(mylist, result=False):
+def arithmetic_arranger(mylist, show=False):
+
     # Limit exceeding error
     if len(mylist) > 5:
         return "Error: Too many problems."
@@ -11,6 +12,7 @@ def arithmetic_arranger(mylist, result=False):
         if '+' in i:
             temp = i.split('+')
             for add in temp:
+                add = add.strip()
                 if len(add) > 4:
                     return "Error: Numbers cannot be more than four digits."
                 try:
@@ -21,6 +23,7 @@ def arithmetic_arranger(mylist, result=False):
         if '-' in i:
             temp = i.split('-')
             for sub in temp:
+                sub = sub.strip()
                 if len(sub) > 4:
                     return "Error: Numbers cannot be more than four digits."
                 try:
@@ -29,5 +32,46 @@ def arithmetic_arranger(mylist, result=False):
                     return "Error: Numbers must only contain digits."
 
     # Working on the perfect data maybe without error ^_^
+    firstLine = ''
+    secondLine = ''
+    dashLine = ''
+    solLine = ''
+
     for single in mylist:
-        pass
+        if "+" in single:
+            data = single.split("+")
+            dataMax = max(int(data[0]), int(data[1]))
+            firstVal = int(data[0])
+            secVal = int(data[1])
+            firstLine += " " * (6 -
+                                len(str(data[0]).strip())) + str(data[0]).strip() + "    "
+
+            secondLine += "+"+" " * (5 -
+                                     len(str(data[1]).strip())) + str(data[1]).strip() + "    "
+            dashLine += ("-" * (2+len(str(dataMax)))) + "    "
+            ans = firstVal + secVal
+            solLine += (" " * (6 - len(str(ans))))+str(ans)+"    "
+
+        elif "-" in single:
+            data = single.split("-")
+            dataMax = max(int(data[0]), int(data[1]))
+            firstVal = int(data[0])
+            secVal = int(data[1])
+            firstLine += " " * (6 -
+                                len(str(data[0]).strip())) + str(data[0]).strip() + "    "
+
+            secondLine += "-"+" " * (5 -
+                                     len(str(data[1]).strip())) + str(data[1]).strip() + "    "
+            dashLine += ("-" * (2+len(str(dataMax)))) + "    "
+            ans = firstVal + secVal
+            solLine += (" " * (6 - len(str(ans))))+str(ans)+"    "
+
+    print(firstLine)
+    print(secondLine)
+    print(dashLine)
+    if show:
+        print(solLine)
+
+
+no = arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"], True)
+print(no)
