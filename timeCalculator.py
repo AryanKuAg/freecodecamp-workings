ed6574e8ced1d27f686cad7d5873dd825b0109d8
Aryan Agrawal <alemantrixaryan@gmail.com>
2022-01-09 20:09:00 +0530

    Creating time calculator

---
+++ timeCalculator.py
@@ -0,0 +1,14 @@
+def add_time(time, duration, day="0"):
+    isAm = True if "AM" in time else False
+    timeHour = time.split(":")[0]
+    timeMinute = time.split(":")[:2]
+    durationHour = duration.split(":")[0]
+    durationMinute = duration.split(":")[:2]
+    # time
+
+    # duration
+
+    # day
+
+    # add_time("3:00 PM", "3:10")
+    # # Returns: 6:10 PM
