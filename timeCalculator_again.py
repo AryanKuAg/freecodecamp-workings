3e974bd0efd690b8fc954235bfa6f5da760074d1
Aryan Agrawal <alemantrixaryan@gmail.com>
2022-01-12 14:46:50 +0530

    part1 is addd

--- timeCalculator_again.py
+++ timeCalculator_again.py
@@ -1,3 +1,7 @@
+dic_day = {"sunday": 0, "monday": 1, "tuesday": 2,
+           "wednesday": 3, "thursday": 4, "friday": 5, "saturday": 6}
+
+
 def add_time(time, duration, day=""):
     day = day.lower()
     # raw to useful
@@ -18,6 +22,28 @@ def add_time(time, duration, day=""):
     # print(calHour, calMinute)
     # above calculation is based on 24-hour model
     ####################################################
+    day_later_cal = calHour // 24
+    day_later_val = ''
+    if day_later_cal == 0:
+        pass
+    elif day_later_cal == 1:
+        day_later_val = "(next day)"
+    elif day_later_cal > 1:
+        day_later_val = f"({day_later_cal} days later)"
+
+    ###########################################
+    if day != "":
+        daynum = dic_day[day]
+        daynum += day_later_cal % 7
+        for key, value in dic_day.items():
+            if value == daynum:
+                day = key.capitalize()
+
+    ############################################
+    part1 = str(calHour).zfill(
+        2) if calHour < 12 else str(calHour % 12).zfill(2) + ":" + str(calMinute).zfill(2) + " " + "AM" if calHour < 12 else "PM"
+    part2 = ''
+    part3 = ''
 
     # (next day) if result is next day
     # (n days later) if result is more than 1 day
@@ -27,5 +53,8 @@ def add_time(time, duration, day=""):
 add_time("3:00 PM", "3:10")
 # # Returns: 6:10 PM
 
-# add_time("11:30 AM", "2:32", "Monday")
-# # Returns: 2:02 PM, Monday
+add_time("11:30 AM", "2:32", "Monday")
+# Returns: 2:02 PM, Monday
+
+# add_time("11:43 PM", "24:20", "tueSday")
+# # Returns: 12:03 AM, Thursday (2 days later)
