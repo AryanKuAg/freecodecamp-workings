05a32d02cb94b932c86dfbdf81227abe973ae98a
Aryan Agrawal <alemantrixaryan@gmail.com>
2022-01-12 19:43:12 +0530

    get_amount_inside is the last thing to work on

--- polygonAreaCalculator.py
+++ polygonAreaCalculator.py
@@ -5,11 +5,11 @@ class Rectangle:
         self.width = width
         self.height = height
 
-    def set_width(self):
-        pass
+    def set_width(self, width):
+        self.width = width
 
-    def set_height(self):
-        pass
+    def set_height(self, height):
+        self.height = height
 
     def get_area(self):
         return self.width * self.height
@@ -23,3 +23,54 @@ class Rectangle:
     def get_picture(self):
         if self.height > 50 or self.width > 50:
             return "Too big for picture."
+        picture = ''
+        for row in range(self.height):
+            for col in range(self.width):
+                picture += "*"
+            picture += "\n"
+        return picture
+
+    def get_amount_inside(self, otherShape):  # future
+        pass
+
+    def __str__(self):
+        return f"Rectangle(width={self.width}, height={self.height})"
+
+
+class Square(Rectangle):
+    def __init__(self, side_length):
+        self.width = side_length
+        self.height = side_length
+
+    def set_side(self, side):
+        self.width = side
+        self.height = side
+
+    def __str__(self):
+        return f"Square(side={self.width})"
+
+    def set_width(self, side):
+        self.width = side
+
+    def set_height(self, side):
+        self.height = side
+
+
+########################################
+# rect = Rectangle(10, 5)
+# print(rect.get_area())
+# rect.set_height(3)
+# print(rect.get_perimeter())
+# print(rect)
+# print(rect.get_picture())
+
+# sq = Square(9)
+# print(sq.get_area())
+# sq.set_side(4)
+# print(sq.get_diagonal())
+# print(sq)
+# print(sq.get_picture())
+
+# rect.set_height(8)
+# rect.set_width(16)
+# print(rect.get_amount_inside(sq))
