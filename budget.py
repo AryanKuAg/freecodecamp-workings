1ddb2c203fd80644d0dd22e73705c2b10fe4be0e
Aryan Agrawal <alemantrixaryan@gmail.com>
2022-01-12 18:57:37 +0530

    Abhi tak to sahi lag raha hai

--- budget.py
+++ budget.py
@@ -1,5 +1,8 @@
+import math
 # create a Category class
 # having ledger list
+
+
 class Category:
     ledger = []
 
@@ -60,3 +63,50 @@ class Category:
         #####TOTAL#####
         zeroTotal = str(total) if "." in str(total) else str(total) + ".00"
         print("Total: ", zeroTotal)
+
+
+###############
+def create_spend_chart(category_list):  # (list) -> string
+    # upto four category
+    cat_withdraw = []
+    total_withdraw = 0
+
+    for i in category_list:
+        funds = 0
+        for j in i.ledger:
+            if j["amount"] < 0:
+                funds += j["amount"]
+        total_withdraw += -funds
+        cat_withdraw.append(-funds)
+
+    withdraw_percentage = []  # useful list here 1 - 10
+    for i in cat_withdraw:
+        data = math.floor(i/total_withdraw) * 10
+        withdraw_percentage.append(data)
+
+    ###########################
+    print("Percentage spent by category")
+    mylist = withdraw_percentage
+    nname = category_list.category
+    for i in range(100, -1, -10):
+        addzero = ''
+        for zero in mylist:
+            if i/10 <= zero:
+                addzero += "o  "
+            else:
+                addzero += "   "
+        print(" " * (3 - len(str(i))) + str(i)+"| " + addzero)
+    print("    -" + '---' * len(mylist))
+
+    mymax = 0
+    for i in nname:
+        mymax = max(mymax, len(i))
+
+    for i in range(mymax):
+        cat_name = ''
+        for name in nname:
+            if len(name) - 1 >= i:
+                cat_name += name[i] + "  "
+            else:
+                cat_name += "   "
+        print(" " * 5+cat_name)
