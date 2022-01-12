ecb4de5cd4a604a42a913b298d57f74f1ec4ec09
Aryan Agrawal <alemantrixaryan@gmail.com>
2022-01-12 17:33:38 +0530

    __str__ added

--- budget.py
+++ budget.py
@@ -22,10 +22,41 @@ class Category:
             return False
 
     def get_balance(self):
-        pass
+        mybudget = 0
+        for i in self.ledger:
+            mybudget += i["amount"]
+        return mybudget
 
-    def transfer(self, amount, budget):
-        pass
+    def transfer(self, amount, other_budget):
+        funds = 0
+        for i in self.ledger:
+            funds += i["amount"]
+        if funds >= amount:
+            self.ledger.append(
+                {"amount": -amount, "description": "Transfer to " + str(other_budget.category)})
+            other_budget.deposit(amount, "Transfer from" + self.category)
+            return True
+        else:
+            return False
 
     def check_funds(self, amount):
-        pass
+        balance = 0
+        for i in self.ledger:
+            balance += i["amount"]
+        if amount > balance:
+            return False
+        else:
+            return True
+
+    def __str__(self):
+        print(self.category.center(30, "*"))
+        total = 0
+        for i in self.ledger:
+            part1 = str(i["description"])[:24]
+            part2 = str(i["amount"]) + \
+                ".00" if "." not in str(i["amount"]) else str(i["amount"])
+            total += i["amount"]
+            print(part1 + str(part2)[:8])
+        #####TOTAL#####
+        zeroTotal = str(total) if "." in str(total) else str(total) + ".00"
+        print("Total: ", zeroTotal)
