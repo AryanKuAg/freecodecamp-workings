8a38c99475648146dbd5ef93cb86be27516d4588
Aryan Agrawal <alemantrixaryan@gmail.com>
2022-01-13 17:35:40 +0530

    Aaisa lag raha hai ki mera mahnah rang laya^_^

--- probabilityCalculator.py
+++ probabilityCalculator.py
@@ -1,5 +1,22 @@
 import random
-import copy
+
+
+def isInList(sublist, mylist):
+    if len(sublist) > len(mylist):
+        return False
+
+    isthere = True
+    for sub in sublist:
+        isIn = False
+        for index, l in enumerate(mylist):
+            if sub in l:
+                isIn = True
+                mylist.pop(index)
+                break
+
+        if not isIn:
+            isthere = False
+    return isthere
 
 
 class Hat:
@@ -14,29 +31,30 @@ class Hat:
         # print(self.contents)
 
     def draw(self, ball_to_draw):  # removes balls randomly from contents and return as a list of strings
-        # ball_to_draw is fixed
-        # self.contents will vary
-        #
-
-        take_out = []
-        total_num_draw = ball_to_draw
-        for i in range(total_num_draw):
-
-            if ball_to_draw > len(self.contents):
-                all_ball_copy = self.contents
-                self.contents = []
-                return all_ball_copy
-            r = random.randrange(0, ball_to_draw, 1)
-            copy = self.contents[r]
-            self.contents.pop(r)
-            ball_to_draw -= 1
-            take_out.append(copy)
-
-        return take_out
+        if ball_to_draw > len(self.contents):
+            return self.contents
+        temp_list = self.contents
+        random.shuffle(temp_list)
+        return temp_list[:ball_to_draw]
 
         # Hat(red=5, orange=4, black=1, blue=0, pink=2, striped=9)
 
+        # This function will return the probability
 
-# This function will return the probability
+
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
-    pass
+    no_of_true = 0
+    for exp in range(num_experiments):  # number of experiments
+        h = hat
+
+        # get random balls #problem: no element there
+        balls = h.draw(num_balls_drawn)
+        expected_balls_list = []  # want these balls in balls
+        for k, v in expected_balls.items():
+            for i in range(v):
+                expected_balls_list.append(k)
+        isContain = isInList(expected_balls_list, balls)
+        if isContain:
+            no_of_true += 1
+
+    return no_of_true / num_experiments
